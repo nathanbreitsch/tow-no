27f4c50d218e5e265524c16c096235bbbf7f69ed
nathan breitsch <nathanbreitsch@gmail.com>
2016-02-20 23:35:51 -0500

    stuff

--- tow_no.ino
+++ tow_no.ino
@@ -9,29 +9,46 @@ Tow No! uses a tilt sensor to determine whether your car is being towed.
 
 -------------*/
 
-int led = D0;
+int tilt_led = D0;
+int towing_led = D3;
 int tilt = A0;
+int reset_in = A2;
 int power = A5;
 
-int isTilting = 0;
-
-
+bool is_tilting = true;
+bool sent_alert = false;
+bool press_reset = false;
 
 void setup() {
 
-
-    pinMode(led, OUTPUT);
+    pinMode(tilt_led, OUTPUT);
+    pinMode(towing_led, OUTPUT);
     pinMode(tilt, INPUT);
+    pinMode(reset_in, INPUT);
     pinMode(power, OUTPUT);
 
     digitalWrite(power, HIGH);
-    digitalWrite(led, LOW);
+    digitalWrite(tilt_led, HIGH);
+    digitalWrite(towing_led, HIGH);
 
 }
 
 
 void loop() {
 
-    isTilting = digitalRead(tilt);
-    digitalWrite(led, isTilting);
+    is_tilting = ( digitalRead(tilt) == HIGH );
+    press_reset = ( digitalRead(reset_in) == HIGH );
+
+    if(is_tilting && !sent_alert){
+        Particle.publish("TowStatus","Towing",60,PUBLIC);
+        sent_alert = true;
+        digitalWrite(towing_led, HIGH);
+    }
+
+    if(press_reset){
+        sent_alert = false;
+        digitalWrite(towing_led, LOW);
+    }
+
+    digitalWrite(tilt_led, is_tilting);
 }
